5c1b8a3be37e19b0901aeb2039bb6eb75bdf03f9
Sohail Sagar (XC-AS/EPE5) <Sagar.Sohail@de.bosch.com>
2025-04-06 00:40:39 +0200

    Add request logging using morgan

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
-const express = require('express') //Import the express module as "express"
+const morgan = require ('morgan');
+const express = require('express'); //Import the express module as "express"
 const app = express(); // This line creates an Express app by calling the express() function. Think of app as your web server that you can add routes and features to.
 const port = process.env.PORT || 3000; // Define the port number your 'app' will run on -> defined in env file
 const helloRoute = require ('./routes/hello') // 4. Import the hello route from the routes folder
 const indexRoute = require ('./routes/index')
+app.use(morgan('dev'));
 //Where is the route folder and hello???
 app.use(express.json()); // Middleware to parse JSON in request bodies
 app.use('/hello', helloRoute); // 6. Register the /hello route with the app
