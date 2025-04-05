25bb959195ed69a264bbf789a6f18ab0453898c4
Sohail Sagar (XC-AS/EPE5) <Sagar.Sohail@de.bosch.com>
2025-04-06 01:41:59 +0200

    debug logger issue - remove logger from hello.js

--- routes/hello.js
+++ routes/hello.js
@@ -1,9 +1,6 @@
 // routes/hello.js
-const logger = require('../logger').logger; // Adjust path if needed
 const express = require('express');
 const router = express.Router();
-
-logger.info(' hello.js logger loaded correctly!');
 
 router.get('/', (req, res) => {
   res.json({ message: 'Hello from the /hello route!' });
